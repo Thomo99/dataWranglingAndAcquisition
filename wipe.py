4ca051544e7ff8b302d054f4bef98720ec48192e
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2024-01-28 09:54:46 +1100

    Input/Output folder

--- wipe.py
+++ wipe.py
@@ -1,6 +1,6 @@
 import csv
 
-csv_filename = "extracted_data.csv"
+csv_filename = "./output/extracted_data.csv"
 with open(csv_filename, 'w', newline='') as file:
             csv_writer = csv.writer(file)
 
